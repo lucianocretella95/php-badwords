ce4792954913ce92e23f03c07fce726c1d09cd56
lucianocretella95 <luciano.cretella@tim.it>
2024-06-10 17:26:10 +0200

    tentativo di bonus

--- dati.php
+++ dati.php
@@ -11,10 +11,15 @@
 <body>
     <?php
     $testo = $_GET["testo"];
+    $censura = $_GET["testo"];
     ?>
     <h1>Dati ricevuti</h1>
     <p>il testo è lungo: </p>
     <p> <?php echo strlen($testo) ?> caratteri</p>
+    <p>il nuovo testo è: </p>
+    <?php
+    echo str_replace("testo", "'", $testo); ?>
+    <p> <?php echo strlen($censura) ?> caratteri </p>
 </body>
 
 </html>
